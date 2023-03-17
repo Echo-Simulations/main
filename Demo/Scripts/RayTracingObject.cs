9744809bc23fb456e8755602a5865f5fa1b205c5
JacobCoberly <112647193+JacobCoberly@users.noreply.github.com>
2023-03-17 18:17:08 -0500

    Added ray tracing demo.
    Ray tracing demo scripts for Unity.

---
+++ Demo/Scripts/RayTracingObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class RayTracingObject : MonoBehaviour
+{
+    public int isListener = 0;
+
+    private void OnEnable()
+    {
+        RayTracingMaster.RegisterObject(this);
+        if (gameObject.tag == "Listener") 
+            isListener = 1;
+    }
+
+    private void OnDisable()
+    {
+        RayTracingMaster.UnregisterObject(this);
+    }
+}
